0f848bfadd91dcaf6efc93b98695346b6df97d62
Sibusiso Labi <sibusiso@quirky30.co.za>
2021-05-08 13:50:35 +0200

    Employee Database added

---
+++ Employees/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+    public class Person
+    {
+        [Key]
+
+        [Display(Name = "Person ID")]
+        public int PersonId { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Of Birth")]
+        public DateTime BirthDate { get; set; }
+    }
+}
